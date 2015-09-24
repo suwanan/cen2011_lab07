4ea28f4da890e7ea4ea8ac0c0db2278166cbed5d
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-09-24 23:12:06 +0700

    Create Array02.java
    ให้นักศึกษาทำการคัดลอก Array พร้อมแสดงผล

---
+++ Array02.java
@@ -0,0 +1,8 @@
+public class Array02 {
+  public static void main(String args[]) {
+      int[] sourceArray = {2, 3, 1, 5, 10};
+      int[] targetArray = new int[sourceArray.length];
+      // จงคัดลอกข้อมูล Array จากนั้นให้แสดงข้อมูลใน Array targetArray
+      
+  }
+}
