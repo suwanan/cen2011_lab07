bc68e122b0effc1bc53f650271d254805a6e2aab
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2015-09-24 22:55:09 +0700

    ไฟล์สำหรับกรณีศึกษา ที่ 1 การวิเคราะห์ตัวเลข
    ให้นักศึกษาเพิ่มโค้ดลงไปเพื่อทำให้โปรแกรมทำงานได้สมบูรณ์ตามวัตถุประสงค์

---
+++ AnalyzeNumbers.java
@@ -0,0 +1,21 @@
+public class AnalyzeNumbers {
+  public static void main(String[] args) {
+    java.util.Scanner input = new java.util.Scanner(System.in); numbers[1]:
+    System.out.print("Enter the number of items: ");
+    int n = input.nextInt();
+    double [] numbers = new double[n];
+    double sum = 0;
+    
+    System.out.print("Enter the numbers: ");
+    // เพิ่มโค้ด
+    
+    
+    double average = sum / n;
+    int count = 0; // The number of elements above average
+    // เพิ่มโค้ด
+    
+    System.out.println("Average is " + average);
+    System.out.println("Number of elements above the average is "
+    + count);
+    }
+}
